46d9fa8fc3e5ca17d8f965a85a4401ec4bc2c8dc
Jose Trespalacios Bedoya <josetrespalaciosbedoya@gmail.com>
2024-05-20 07:09:35 -0500

    Base user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function index()
+    {
+        $users = $this->userService->getAllUsers();
+        return response()->json($users);
+    }
+}
