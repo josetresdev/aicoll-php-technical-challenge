84270027ee748b26177c77fb173df410d727159e
Jose Trespalacios Bedoya <josetrespalaciosbedoya@gmail.com>
2024-05-20 13:42:10 -0500

    Seed data implementation

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Company;
+use Faker\Factory as Faker;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            Company::create([
+                'name' => $faker->company,
+                'nit' => $faker->unique()->numerify('##########'), // Generate a 10 digit NIT
+                'status' => $faker->randomElement(['active', 'inactive']),
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password'), // Hashing a default password
+            ]);
+        }
+    }
+}
